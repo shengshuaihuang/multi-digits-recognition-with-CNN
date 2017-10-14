0c25a70e885c193f816470c6f92b2969e2a8a221
hsshuai <hsshuai1221@foxmail.com>
2017-10-14 16:16:34 +0800

    initial project

--- train.py
+++ train.py
@@ -15,21 +15,21 @@ epochs = 20
 
 
 def convmodel():
-    model = Sequential()
-    model.add(Conv2D(30, (5, 5), activation='relu', padding='valid', input_shape=(1, 55, 30), name='conv1'))
-    model.add(MaxPooling2D((2, 2), name='pool1'))
+	model = Sequential()
+	model.add(Conv2D(30, (5, 5), activation='relu', padding='valid', input_shape=(1, 55, 30), name='conv1'))
+	model.add(MaxPooling2D((2, 2), name='pool1'))
 
-    model.add(Conv2D(15, (3, 3), activation='relu',  name='conv2'))
-    model.add(MaxPooling2D((2, 2), name='pool2'))
+	model.add(Conv2D(15, (3, 3), activation='relu',  name='conv2'))
+	model.add(MaxPooling2D((2, 2), name='pool2'))
 
-    model.add(Dropout(0.2))
-    model.add(Flatten(name='flatten'))
-    model.add(Dense(128, activation='relu', name='fc1'))
-    model.add(Dense(50, activation='relu', name='fc2'))
-    model.add(Dense(10, activation='softmax', name='predictions'))
-    # Compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+	model.add(Dropout(0.2))
+	model.add(Flatten(name='flatten'))
+	model.add(Dense(128, activation='relu', name='fc1'))
+	model.add(Dense(50, activation='relu', name='fc2'))
+	model.add(Dense(10, activation='softmax', name='predictions'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
 
 
 # the data, shuffled and split between train and test sets
@@ -59,13 +59,13 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = convmodel()
 history = model.fit(X_train, Y_train,
-                    batch_size=batch_size, epochs=epochs,
-                    verbose=1, validation_data=(X_test, Y_test))
+					batch_size=batch_size, epochs=epochs,
+					verbose=1, validation_data=(X_test, Y_test))
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
 model_json = model.to_json()
-with open("./Network/model/digit_CNN_2.json", "w") as json_file:
-    json_file.write(model_json)
-model.save_weights("./Network/model/digit_CNN_2.h5")
+with open("./Network/model/digit_CNN.json", "w") as json_file:
+	json_file.write(model_json)
+model.save_weights("./Network/model/digit_CNN.h5")
 print("Saved model to disk")
